4914a635ccaa01bcde9563d78011a81939060c1a
rezamf17 <rezayuyara@gmail.com>
2022-01-10 22:40:25 +0700

    validasi form tambah akun

--- app/Http/Controllers/KelolaAkunController.php
+++ app/Http/Controllers/KelolaAkunController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class KelolaAkunController extends Controller
 {
@@ -37,6 +38,31 @@ class KelolaAkunController extends Controller
      */
     public function store(Request $request)
     {
+        $rules = [
+            'id_cabang' => 'required',
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'role' => 'required',
+            'password' => 'required|confirmed|min:8',
+        ];
+
+        $messages = [
+            'id_cabang.required' => 'Kantor Cabang tidak boleh kosong',
+            'name.required' => 'Nama tidak boleh kosong',
+            'name.string' => 'Nama harus berupa huruf',
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Email harus email yang valid',
+            'role.required' => 'Level tidak boleh kosong',
+            'password.required' => 'Password tidak boleh kosong',
+            'password.confirmed' => 'Konfirmasi Password harus sama',
+            'password.min' => 'Password minimal 8 karakter',
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+         return redirect('/KelolaAkun')->with('warning', $validator->errors()->first());
+        }
+
         $user = new User;
         $user->id_cabang = $request->id_cabang;
         $user->name = $request->name;
@@ -81,6 +107,30 @@ class KelolaAkunController extends Controller
     public function update(Request $request, $id)
     {
         $user = User::find($id);
+        $rules = [
+            'id_cabang' => 'required',
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'role' => 'required',
+            'password' => 'confirmed',
+        ];
+
+        $messages = [
+            'id_cabang.required' => 'Kantor Cabang tidak boleh kosong',
+            'name.required' => 'Nama tidak boleh kosong',
+            'name.string' => 'Nama harus berupa huruf',
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Email harus email yang valid',
+            'role.required' => 'Level tidak boleh kosong',
+            'password.confirmed' => 'Konfirmasi Password harus sama',
+            'password.min' => 'Password minimal 8 karakter',
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+         return redirect('/KelolaAkun/'.$user->id.'/edit')->with('warning', $validator->errors()->first());
+        }
+
         if ($request->password == null) {
             $user->id_cabang = $request->id_cabang;
             $user->name = $request->name;
